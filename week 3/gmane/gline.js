3cfca12d8e0b0cf773f122952523f2cebc0aa559
Rahul Patra <excuseme17011996@users.noreply.github.com>
2016-12-04 23:36:42 +0530

    Add files via upload

---
+++ week 3/gmane/gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Month','unicon.net','cam.ac.uk','northwestern.edu','mtu.edu','rsmart.com','umich.edu','virginia.edu','yahoo.com','asu.edu','columbia.edu'],
+['2005-12',4,3,2,2,2,2,2,1,1,1]
+];
